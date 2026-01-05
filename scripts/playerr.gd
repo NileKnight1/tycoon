280d80604565772361e93843c6227bccd26a444b
Ammar Waleed <ammarwaleed@gmail.com>
2026-01-05 12:02:36 +0200

    after hour and half, i made basic map tiles, and im still trying ti adjust collisions for the body with tiles

---
+++ scripts/playerr.gd
@@ -0,0 +1,67 @@
+extends CharacterBody2D
+
+
+@export var SPEED = global.player_speed
+@export var boost = global.player_boost
+
+
+@onready var player = $ch
+#const JUMP_VELOCITY = -400.0
+
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	#if not is_on_floor():
+	velocity += get_gravity() * delta
+
+	var direction := Input.get_axis("left", "right")
+	var direction2 := Input.get_axis("up", "down")
+	var boost_check = Input.is_action_pressed("sprint")
+	
+	if direction:
+		if boost_check:
+			velocity.x = direction * SPEED * boost
+		else:
+			velocity.x = direction * SPEED
+		#player.play("move")
+		#print("hori")
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#player.play("default")
+		
+	if direction2:
+		if boost_check:
+			velocity.y = direction2 * SPEED * boost
+		else:
+			velocity.y = direction2 * SPEED
+		#if direction2<0:
+			##player.play("up")
+			#print("Up")
+		#else: 
+			##player.play("down")
+			#print("Down")
+			
+		#print("im moving V")
+	else:
+		#player.play("default")
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	#print(direction)
+	#print(direction2)
+	if direction > 0:
+		player.flip_h = false
+	elif direction < 0:
+		player.flip_h = true
+	
+	if direction != 0:
+		player.play("move")
+	elif direction2 == -1:
+		player.play("up")
+	elif direction2 == 1:
+		player.play("down")
+	else:
+		player.play("default")
+
+
+	move_and_slide()
